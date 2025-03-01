88a7830722efd2f2ca70ef2b37fdd2203d7bc0e5
Dikshit Shrestha <work.dikxitshrestha99@gmail.com>
2025-03-01 18:16:42 +0545

    changed page title

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -34,7 +34,7 @@ export default function Hero() {
             </a>
             <a
               href="https://docs.google.com/document/d/1HaYrEYk_AxqG0B9IHUUfaxxPtcpEdoGa/edit?usp=drive_link&ouid=102911768468676265891&rtpof=true&sd=true"
-              className="font-semibold text-[17px] flex items-center gap-1 text-red-500"
+              className="font-semibold text-[17px] flex items-center gap-1 text-red-500 hover:text-red-700 ease-in-out duration-300"
               target="_blank"
             >
               My CV <LuSquareArrowOutUpRight />
